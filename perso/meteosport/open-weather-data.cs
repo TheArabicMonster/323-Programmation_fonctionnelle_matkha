bcec96a12dee1dd06636467038d1e6e53f18da05
TheArabicMonster <102302877+TheArabicMonster@users.noreply.github.com>
2024-10-02 12:05:20 +0200

    Déplacement exo personnel dans le dossier pero

---
+++ perso/meteosport/open-weather-data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meteosport
+{
+    public class open_weather_data
+    {
+
+    }
+}
