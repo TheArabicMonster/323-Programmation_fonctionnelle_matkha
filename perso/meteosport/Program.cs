41e6af060b8ddcef0eb181ddbf3f039eb8f975fa
TheArabicMonster <neetamk@icloud.com>
2024-10-06 21:49:24 +0200

    exo weather data

--- perso/meteosport/Program.cs
+++ perso/meteosport/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -17,6 +19,48 @@ public class Program
         var response = await client.GetStringAsync(url);
         var weatherData = JsonConvert.DeserializeObject<WeatherData>(response);
 
-        // Utilisez weatherData pour les étapes suivantes...
+        var bestRunningHours = GetBestRunningHours(weatherData.hourly);
+
+        foreach (var hour in bestRunningHours)
+        {
+            Console.WriteLine($"Time: {hour.Time}, Temperature: {hour.Temperature}, Wind Speed: {hour.WindSpeed}");
+        }
+
+        var averages = CalculateAverages(weatherData.hourly);
+        Console.WriteLine($"Average Temperature: {averages.AverageTemperature}, Average Wind Speed: {averages.AverageWindSpeed}");
+
+        var idealSportHoursCount = CountIdealSportHours(weatherData.hourly);
+        Console.WriteLine($"Number of Ideal Sport Hours: {idealSportHoursCount}");
+    }
+
+    public static IEnumerable<(string Time, float Temperature, float WindSpeed)> GetBestRunningHours(Hourly hourly)
+    {
+        return hourly.time
+            .Select((t, index) => new { Time = t, Temperature = hourly.temperature_2m[index], Precipitation = hourly.precipitation[index], WindSpeed = hourly.wind_speed_10m[index] })
+            .Where(x => x.Temperature >= 18 && x.Temperature <= 22 && x.Precipitation == 0 && x.WindSpeed < 10)
+            .Select(x => (x.Time, x.Temperature, x.WindSpeed));
+    }
+
+    public static (float AverageTemperature, float AverageWindSpeed) CalculateAverages(Hourly hourly)
+    {
+        var averageTemperature = hourly.temperature_2m.Average();
+        var averageWindSpeed = hourly.wind_speed_10m.Average();
+
+        return (averageTemperature, averageWindSpeed);
+    }
+
+    public static int CountIdealSportHours(Hourly hourly)
+    {
+        return hourly.time
+            .Select((t, index) => new { Temperature = hourly.temperature_2m[index], Precipitation = hourly.precipitation[index], WindSpeed = hourly.wind_speed_10m[index] })
+            .Aggregate(0, (count, hour) =>
+            {
+                // Incrémenter le compteur si les conditions sont idéales
+                if (hour.Temperature >= 15 && hour.Temperature <= 25 && hour.Precipitation == 0 && hour.WindSpeed < 15)
+                    return count + 1;
+                else
+                    return count;
+            });
     }
 }
+
